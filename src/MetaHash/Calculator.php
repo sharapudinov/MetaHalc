aeeadebad9486911819593749432fb2675885423
sharapudinov@mail.ru <e2e4shamil>
2019-04-27 08:02:37 +0300

    scale optimization

--- src/MetaHash/Calculator.php
+++ src/MetaHash/Calculator.php
@@ -5,7 +5,7 @@ namespace MetaHash;
 
 class Calculator
 {
-    const SCALE = 10;
+    const SCALE = 6;
 
     const NUMBER_OF_RANDOM_REWARD_WINNERS = 1000;
     const WALLET_FORGING_DEPOSIT_VALUE = 101;
@@ -23,54 +23,50 @@ class Calculator
 
     public static function randomRewardMatrix($total, $own)
     {
+        bcscale(self::SCALE);
         $total = (int)$total;
         $own = (int)$own;
         $total_profit=0;
         $total_ROI=0;
-        $frozen_amount=bcmul(self::WALLET_FORGING_DEPOSIT_VALUE,$own);
+        $frozen_amount=bcmul(self::WALLET_FORGING_DEPOSIT_VALUE,$own,0);
 
         foreach (self::REWARD_WIN_PLACES_DATA as $place => $place_data) {
             $opposite_probability = bcdiv(
                 gmp_binomial($total - $own, $place_data['winners']),
-                gmp_binomial($total, $place_data['winners']),
-                self::SCALE
+                gmp_binomial($total, $place_data['winners'])
             );
             $probability=bcsub(
                 1,
-                $opposite_probability,
-                self::SCALE
+                $opposite_probability
             );
-            $place_matrix[$place]['PROBABILITY'] = bcmul($probability,100,self::SCALE);
+            $place_matrix[$place]['PROBABILITY'] = bcmul($probability,100,self::SCALE-2);
 
             $place_matrix[$place]['AVG_DAILY_PROFIT'] = bcmul(
                 $probability,
-                self::DAILY_REWARD_POOL * $place_data['percent'] / 100,
-                self::SCALE
+                self::DAILY_REWARD_POOL * $place_data['percent'] / 100
             );
 
-            $place_matrix[$place]['PERIODICITY']=bcdiv(1, $probability);
+            $place_matrix[$place]['PERIODICITY']=bcdiv(1, $probability,0);
 
             $daili_roi=bcdiv(
                 $place_matrix[$place]['AVG_DAILY_PROFIT'],
-                $frozen_amount,
-                self::SCALE
+                $frozen_amount
             );
-            $place_matrix[$place]['DAILY_ROI'] = bcmul($daili_roi,100,self::SCALE);
-            $total_profit = bcadd($total_profit,$place_matrix[$place]['AVG_DAILY_PROFIT'],self::SCALE);
-            $total_ROI=bcadd($total_ROI,$place_matrix[$place]['DAILY_ROI'],self::SCALE);
+            $place_matrix[$place]['DAILY_ROI'] = bcmul($daili_roi,100,self::SCALE-2);
+            $total_profit = bcadd($total_profit,$place_matrix[$place]['AVG_DAILY_PROFIT']);
+            $total_ROI=bcadd($total_ROI,$place_matrix[$place]['DAILY_ROI'],self::SCALE-1);
 
         }
         $result['PLACE_MATRIX']=$place_matrix;
         $total_opposite_probability = bcdiv(
             gmp_binomial($total - $own, self::NUMBER_OF_RANDOM_REWARD_WINNERS),
             gmp_binomial($total, self::NUMBER_OF_RANDOM_REWARD_WINNERS),
-            self::SCALE
         );
-        $total_probability= bcsub(1,$total_opposite_probability,self::SCALE);
+        $total_probability= bcsub(1,$total_opposite_probability);
         $result['PLACE_MATRIX'][7]=[
-            'PROBABILITY' =>bcmul($total_probability,100,self::SCALE),
+            'PROBABILITY' =>bcmul($total_probability,100,self::SCALE-2),
             'AVG_DAILY_PROFIT'=> $total_profit,
-            'PERIODICITY' => bcdiv(1, $total_probability),
+            'PERIODICITY' => bcdiv(1, $total_probability,0),
             'DAILY_ROI'=>$total_ROI
         ];
         $result['FROZEN_AMOUNT']=$frozen_amount;
