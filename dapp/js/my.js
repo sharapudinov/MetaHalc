4da02df840fa946103512588f4ac3067e3e40893
sharapudinov@mail.ru <e2e4shamil>
2019-04-29 23:32:18 +0300

    version 1.0.1 adaptive navBar adjust

--- dapp/js/my.js
+++ dapp/js/my.js
@@ -2,8 +2,24 @@ $(function () {
     var navMenuItem=$('#navMenu .navbar-item');
 
     navMenuItem.on('click',router);
+    navMenuItem.on('click',function (e) {
+        if( $(".navbar-burger").hasClass('is-active')){
+            $(".navbar-burger").toggleClass("is-active");
+            $(".navbar-menu").toggleClass("is-active");
+        }
+    });
 
     $(navMenuItem.toArray()[0]).trigger('click');
+
+
+        // Check for click events on the navbar burger icon
+        $(".navbar-burger").on('click',function() {
+            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+            $(".navbar-burger").toggleClass("is-active");
+            $(".navbar-menu").toggleClass("is-active");
+
+        });
+
 
 });
 
